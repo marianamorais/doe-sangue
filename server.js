00e6bc535cbdc1c15da77f1202ecae58247945dc
Mariana <moraismariana83@gmail.com>
2020-02-20 16:32:21 -0300

    Server

--- server.js
+++ server.js
@@ -3,13 +3,17 @@
 const express = require("express");
 const server = express();
 
-// configurar o servidor para apresentar arquivos extras
+// configurar o servidor para apresentar arquivos estaticos
 server.use(express.static('public'));
+
+// habilitar o body do formulario
+server.use(express.urlencoded({ extended: true }))
 
 // configurando a template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("./", {
- express: server
+ express: server,
+ noCache: true, //boolean
 });
 
 //Lista de doadores: Vetor ou Array
@@ -18,14 +22,17 @@ const donors = [
   name: "Mariana Morais",
   blood: "AB+"
  },
+
  {
   name: "Victor",
   blood: "B+"
  },
+
  {
   name: "Diego",
   blood: "A+"
  },
+
  {
   name: "Larissa",
   blood: "O+"
@@ -34,8 +41,23 @@ const donors = [
 
 //configurar a apresetaçao da pagina
 server.get("/", function(req, res) {
- return res.render("index.html", { valor: "1" });
+ return res.render("index.html", { donors });
 });
+
+//Receber os dados do formulário
+server.post("/", function(req, res) {
+ const name = req.body.name
+ const email = req.body.email
+ const blood = req.body.blood
+
+ donors.push({ 
+  name: name,
+  blood: blood,
+ })
+
+ return res.redirect("/")
+ 
+ });
 
 //ligar o servidor e permitir acesso na porta
 server.listen(3000, function() {
